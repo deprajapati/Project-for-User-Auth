1fc8dbdfc04e2b9da1707bd1a2353e1da3e41e53
deprajapati <devanshprajapati31@gmail.com>
2023-05-27 11:08:01 +0530

    predict_update

--- code/app.py
+++ code/app.py
@@ -6,11 +6,59 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import scoped_session
 from project_orm import User
 from utils import *
+import pandas as pd
+import numpy as np
+import joblib
 
 from flask import Flask,session,flash,redirect,render_template,url_for
 
 app = Flask(__name__)
 app.secret_key = "the basics of life with python"
+
+def load_xgb_model():
+    xgb_r = joblib.load('training/xgb_r.pkl')
+    return xgb_r
+
+def make_df(team, targeted_productivity, smv, wip, over_time, 
+            incentive, idle_time, idle_men, no_of_style_change, 
+            no_of_workers, month, quarter_Quarter1, quarter_Quarter2, 
+            quarter_Quarter3, quarter_Quarter4, quarter_Quarter5,
+            day_Sunday, 
+            day_Monday, 
+            day_Tuesday, 
+            day_Wednesday,
+            day_Thursday, 
+            day_Saturday, 
+        ):
+    dfinp = pd.DataFrame({
+        'team' : [team],
+        'targeted_productivity' : [targeted_productivity],
+        'smv' : [smv],
+        'wip' : [wip],
+        'over_time' : [over_time],
+        'incentive' : [incentive],
+        'idle_time' : [idle_time],
+        'idle_men': [idle_men],
+        'no_of_style_change' : [no_of_style_change],
+        'no_of_workers' : [no_of_workers],
+        'month' : [month],
+        'quarter_Quarter1' : [quarter_Quarter1],
+        'quarter_Quarter2' : [quarter_Quarter2],
+        'quarter_Quarter3' : [quarter_Quarter3],
+        'quarter_Quarter4' : [quarter_Quarter4],
+        'quarter_Quarter5' : [quarter_Quarter5],
+        'day_Monday' : [day_Monday],
+        'day_Saturday' : [day_Saturday],
+        'day_Sunday' : [day_Sunday],
+        'day_Thursday' : [day_Thursday],
+        'day_Tuesday' : [day_Tuesday],
+        'day_Wednesday' : [day_Wednesday]
+    })
+    return dfinp
+def predict_performance(inp):
+    model = load_xgb_model()
+    return model.predict(inp)[0] * 100
+
 
 def get_db():
     engine = create_engine('sqlite:///database.db')
@@ -80,7 +128,45 @@ def forgot():
 def home():
     if session.get('isauth'):
         username = session.get('name')
+        if request.method == 'POST':
+            print(request.form)
+            team = request.form.get('team')
+            trageted_productivity = request.form.get('trageted_productivity')
+            smv = request.form.get('smv')
+            wip = request.form.get('wip')
+            over_time = request.form.get('over_time')
+            inceptive = request.form.get('inceptive')
+            idle_time = request.form.get('idle_time')
+            idle_men = request.form.get('idle_men')
+            no_of_style_change = request.form.get('no_of_style_change')
+            no_of_workers = request.form.get('no_of_workers')
+            month = request.form.get('month')
+            quarter_Quarter1 = request.form.get('quarter_Quarter1')
+            quarter_Quarter2 = request.form.get('quarter_Quarter2')
+            quarter_Quarter3 = request.form.get('quarter_Quarter3')
+            quarter_Quarter4 = request.form.get('quarter_Quarter4')
+            quarter_Quarter5 = request.form.get('quarter_Quarter5')
+            day_Sunday = request.form.get('day_Sunday')
+            day_Monday = request.form.get('day_Monday')
+            day_Tuesday = request.form.get('day_Tuesday')
+            day_Wednesday = request.form.get('day_Wednesday')
+            day_Thursday = request.form.get('day_Thursday')
+            day_Saturday = request.form.get('day_Saturday')
+
+            try:
+                df = make_df(team, trageted_productivity, smv, wip, over_time,
+                               inceptive, idle_time, idle_men, no_of_style_change,
+                               no_of_workers, month, quarter_Quarter1, quarter_Quarter2,
+                                quarter_Quarter3, quarter_Quarter4, quarter_Quarter5,
+                                day_Sunday, day_Monday, day_Tuesday, day_Wednesday,
+                                day_Thursday, day_Saturday)
+                performance = predict_performance(df)
+                flash(f'predicted performance is {performance}','success')
+                return render_template('home.html',title=f'Home|{username}',performance=performance)
+            except Exception as e:
+                flash(e,'danger')    
         return render_template('home.html',title=f'Home|{username}')
+
     flash('please login to continue','warning')
     return redirect('/')
 
